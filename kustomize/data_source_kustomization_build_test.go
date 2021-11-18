51e81539e865201b3539895cd0f93c7136d904ef
Bradon Kanyid <rattboi@gmail.com>
2021-11-17 17:39:09 -0800

    Initial datasource kustomization_build test + chart

--- kustomize/data_source_kustomization_build_test.go
+++ kustomize/data_source_kustomization_build_test.go
@@ -69,3 +69,52 @@ data "kustomization" "test" {
 }
 `, path)
 }
+
+func TestAccDataSourceKustomization_helmChart(t *testing.T) {
+
+	resource.Test(t, resource.TestCase{
+		//PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceKustomizationConfig_helm("test_kustomizations/helm/initial", false),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.kustomization_build.test", "id"),
+					resource.TestCheckResourceAttrSet("data.kustomization_build.test", "path"),
+					resource.TestCheckResourceAttr("data.kustomization_build.test", "path", "test_kustomizations/helm/initial"),
+					resource.TestCheckResourceAttr("data.kustomization_build.test", "ids.#", "2"),
+					resource.TestCheckResourceAttr("data.kustomization_build.test", "ids_prio.#", "3"),
+					resource.TestCheckResourceAttr("data.kustomization_build.test", "manifests.%", "2"),
+					resource.TestCheckOutput("service", "{\"apiVersion\":\"v1\",\"kind\":\"Service\",\"metadata\":{\"labels\":{\"name\":\"redis\"},\"name\":\"redis\"},\"spec\":{\"ports\":[{\"port\":6379,\"targetPort\":6379}],\"selector\":{\"name\":\"redis\"}}}"),
+					resource.TestCheckOutput("deployment", "{\"apiVersion\":\"apps/v1\",\"kind\":\"Deployment\",\"metadata\":{\"labels\":{\"name\":\"redis\",\"release\":\"RELEASE-NAME\"},\"name\":\"redis\"},\"spec\":{\"replicas\":1,\"selector\":{\"matchLabels\":{\"name\":\"redis\"}},\"template\":{\"metadata\":{\"labels\":{\"name\":\"redis\"}},\"spec\":{\"containers\":[{\"image\":\"redis:6.0.10\",\"imagePullPolicy\":\"IfNotPresent\",\"name\":\"simple-redis\",\"ports\":[{\"containerPort\":6379,\"name\":\"redis-port\",\"protocol\":\"TCP\"}],\"resources\":{\"limits\":{\"cpu\":\"100m\",\"memory\":\"128Mi\"},\"requests\":{\"cpu\":\"100m\",\"memory\":\"128Mi\"}}}]}}}}"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceKustomizationConfig_helm(path string, legacy bool) string {
+	return fmt.Sprintf(`
+
+provider "kustomization" {
+	legacy_id_format = %v
+}
+
+data "kustomization_build" "test" {
+	path = "%s"
+
+	kustomize_options = {
+		enable_helm = true
+		helm_path = "helm"
+	}
+}
+
+output "service" {
+	value = data.kustomization_build.test.manifests["_/Service/_/redis"]
+}
+
+output "deployment" {
+	value = data.kustomization_build.test.manifests["apps/Deployment/_/redis"]
+}
+`, legacy, path)
+}
